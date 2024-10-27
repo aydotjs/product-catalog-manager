9e06f40896e5d3bbb435a9ed2e952e63e9e6cadb
aydotjs <ayobami66@gmail.com>
2024-10-27 23:44:51 +0100

    handling post request to create product data

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import ProductListView, CustomerListView, CustomerCreate
+from .views import ProductListView, CustomerListView, CustomerCreate, ProductCreate
 
 urlpatterns = [
     path('products/', ProductListView.as_view()),
     path("customers/", CustomerListView.as_view()),
-    path("customers/create/", CustomerCreate.as_view())
+    path("customers/create/", CustomerCreate.as_view()),
+    path("products/create/", ProductCreate.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics  
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product, Customer
 from .serializers import ProductSerializer, CustomerSerializer
+
 
 class ProductListView(APIView):
     def get(self, request):
@@ -11,12 +12,19 @@ class ProductListView(APIView):
         serializer = ProductSerializer(products, many=True)  # Serialize the data
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 class CustomerListView(APIView):
     def get(self, request):
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
 
+
 class CustomerCreate(generics.CreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+
+class ProductCreate(generics.CreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = ProductSerializer

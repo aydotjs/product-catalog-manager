f8d1bf3ef6b0cccc1162cdcde2a6a727597669c7
aydotjs <ayobami66@gmail.com>
2024-10-27 22:06:29 +0100

    readme

--- pcm/settings.py
+++ pcm/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'pcm.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'product_catalogue_manager',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '3306',
+}
+
 }
 
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+ Documentation Summary
+🎯 Virtual Environment: Use a virtual environment for dependency management.
+📥 Install Packages: Install Django and MySQL client.
+🏗️ Project & Models: Set up Django project and define models.
+💾 Database: Create a MySQL database and configure settings.py.
+🔄 Migrations: Use makemigrations and migrate to create tables.
+🔑 Admin User: Create a superuser for the admin dashboard.
+🚀 Run Server: Start the server to access the project.
